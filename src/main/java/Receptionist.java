c3258655b8d9633a7fcf943ecbbf1656ac2e64d2
Steven Orozco <orozcosteven179@gmail.com>
2024-04-08 12:54:08 -0700

    HealthcareProvider, Message, and Receptionist class

---
+++ src/main/java/Receptionist.java
@@ -0,0 +1,22 @@
+public class Receptionist {
+
+    //needs implementation
+    public static void createPaitentID() {
+
+    }
+
+    //needs implementation
+    public static void createProviderID() {
+
+    }
+
+    //needs changes to implemtation
+    public static Patient createPatient(PatientInfoForm form) {
+        Patient newPatient = new Patient(null, new PatientRecord(form), null); //Fillout ID and initalVisit with relevant information
+        return newPatient;
+    }
+
+    public static HealthcareProvider createHealthcareProvider() {
+        
+    }
+}
