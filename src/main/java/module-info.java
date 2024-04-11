778f83cb0e0c846012d2ef5b59ab5c13709a3b17
vicvala <vicvala1919@gmail.com>
2024-04-10 18:00:41 -0700

    added UI for new patient form

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.cse360.medicalproject.projectphaseii {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
 
     opens com.cse360.medicalproject.projectphaseii to javafx.fxml;
